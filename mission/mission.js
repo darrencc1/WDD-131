e37e216b1161df0246844a422fbd6de0c57ff808
darrencc1 <156107616+darrencc1@users.noreply.github.com>
2026-04-30 15:32:51 -0600

    Mission updated and finished Prove 2

---
+++ mission/mission.js
@@ -0,0 +1,24 @@
+const background = document.querySelector("body");
+// const wordCol = document.querySelector("p");
+let logo = document.querySelector("img");
+let mode = document.querySelector('select');
+// console.log(logo);
+// console.log(mode);
+mode.addEventListener('change', changeMode);
+
+function changeMode() {
+    let current = mode.value;
+    if (current == 'dark') {
+        // background.style.backgroundColor = 'black';
+        // wordCol.style.color = 'white';
+        background.classList.add("dark-mode");
+        // Since dark-mode is added to the body as a class. Everything will inherit its css rules. 
+        logo.setAttribute('src', 'byui-logo-dark.png');
+    }
+    else {
+        // background.style.backgroundColor = 'white';
+        background.classList.remove("dark-mode");
+        logo.setAttribute('src', 'byui-logo.webp');
+
+    }
+};
